eafdd3cb2740c9e3c6b52f365a2e91c1e30badb3
Obinna Iwuiji <obiwuji@Obinnas-MacBook-Pro.local>
2022-08-28 22:45:07 +0100

    changed formatting/colours and added scroll trigger

--- public/javascript/timeline.js
+++ public/javascript/timeline.js
@@ -1,47 +1,35 @@
 /* Main navigation */
-// let panelsSection = document.querySelector("#panels"),
-// 	panelsContainer = document.querySelector("#panels-container"),
-// 	tween;
-// document.querySelectorAll(".anchor").forEach(anchor => {
-// 	anchor.addEventListener("click", function(e) {
-// 		e.preventDefault();
-// 		let targetElem = document.querySelector(e.target.getAttribute("href")),
-// 			y = targetElem;
-// 		if (targetElem && panelsContainer.isSameNode(targetElem.parentElement)) {
-// 			let totalScroll = tween.scrollTrigger.end - tween.scrollTrigger.start,
-// 				totalMovement = (panels.length - 1) * targetElem.offsetWidth;
-// 			y = Math.round(tween.scrollTrigger.start + (targetElem.offsetLeft / totalMovement) * totalScroll);
-// 		}
-// 		gsap.to(window, {
-// 			scrollTo: {
-// 				y: y,
-// 				autoKill: false
-// 			},
-// 			duration: 1
-// 		});
-// 	});
-// });
-//
-// /* Panels */
-// const panels = gsap.utils.toArray("#panels-container .panel");
-// tween = gsap.to(panels, {
-// 	xPercent: -100 * ( panels.length - 1 ),
-// 	ease: "none",
-// 	scrollTrigger: {
-// 		trigger: "#panels-container",
-// 		pin: true,
-// 		start: "top top",
-// 		scrub: 1,
-// 		snap: {
-// 			snapTo: 1 / (panels.length - 1),
-// 			inertia: false,
-// 			duration: {min: 0.1, max: 0.1}
-// 		},
-// 		end: () =>  "+=" + (panelsContainer.offsetWidth - innerWidth)
-// 	}
-// });
+//Smooth Snap between full-screen sections
+gsap.utils.toArray(".full-screen").forEach((panel, i) => {
+  ScrollTrigger.create({
+    trigger: panel,
+    start: "top top",
+    pin: true,
+    pinSpacing: false
+  });
+});
 
-// Set up our scroll trigger
+
+ScrollTrigger.create({
+  snap: 1 / 16 // snap whole page to the closest section!
+});
+
+//Scroll trigger for chart animation
+gsap.from("#test rect.bars", {
+  ScrollTrigger: {
+    trigger: ".viz-content",
+    start: "top top",
+    scrub: true,
+  },
+  duration: 1,
+  opacity: 0,
+  x:0,
+  stagger:0.25,
+  ease: "none"
+});
+
+
+// Set up our scroll trigger for timeline scrolly
 const ST = ScrollTrigger.create({
   trigger: ".content-container",
   start: "top top",
