625b8571b38affaac495bcf24345d66df7313adb
Obinna Iwuiji <obiwuji@Obinnas-MacBook-Pro.local>
2022-08-29 08:27:04 +0100

    First commit

---
+++ javascript/barviz.js
@@ -0,0 +1,76 @@
+var margin = {top: 20, right: 30, bottom: 40, left: 150},
+    width = 800 - margin.left - margin.right,
+    height = 800 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#bar_viz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .attr("id", "test")
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Parse the Data
+d3.csv("/javascript/African Independence timeline - Colonizer List.csv", function(data) {
+
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([0, 25])
+    .range([ 0, (width)]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .attr("class", "X-axis")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+  // Y axis
+  var y = d3.scaleBand()
+    .range([ 0, (height) ])
+    .domain(data.map(function(d) { return d.Colonizer; }))
+    .padding(.1);
+  svg.append("g")
+    .attr("class", "Y-axis")
+    .call(d3.axisLeft(y))
+
+  var tooltip = d3.select("#bar_viz")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .style("padding", "10px")
+        .style("background-color", "white")
+        .style("border-radius", "4px")
+
+  //Bars
+  svg.selectAll("myRect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("class", "bars")
+    .attr("x", x(0) )
+    .attr("y", function(d) { return y(d.Colonizer); })
+    .attr("width", function(d) { return x(d.Number_of_colonies); })
+    .attr("height", y.bandwidth() )
+    .attr("fill", "#800080")
+    .on("mouseover", function (d,i) {
+  tooltip
+    .html(`<div>Colonizer: ${d.Colonizer}</div><div># of Colonies: ${d.Number_of_colonies}</div>`)
+    .style('visibility', 'visible');
+    d3.select(this).transition().attr("fill","#F7931E");
+  })
+    .on("mousemove", function() {
+        tooltip
+            .style('top', d3.event.pageY - 10 + 'px')
+            .style('left', d3.event.pageX + 10 + 'px');
+          })
+    .on("mouseleave", function() {
+      tooltip.html(``).style('visibility', 'hidden');
+      d3.select(this).transition().attr("fill","#800080");
+    });
+
+})
---
+++ javascript/timeline.js
@@ -0,0 +1,107 @@
+/* Main navigation */
+//Smooth Snap between full-screen sections
+gsap.utils.toArray(".full-screen").forEach((panel, i) => {
+  ScrollTrigger.create({
+    trigger: panel,
+    start: "top top",
+    pin: true,
+    pinSpacing: false
+  });
+});
+
+
+ScrollTrigger.create({
+  snap: 1 / 16 // snap whole page to the closest section!
+});
+
+//Scroll trigger for chart animation
+gsap.from("#test rect.bars", {
+  ScrollTrigger: {
+    trigger: ".viz-content",
+    start: "top top",
+    scrub: true,
+  },
+  duration: 1,
+  opacity: 0,
+  x:0,
+  stagger:0.25,
+  ease: "none"
+});
+
+
+// Set up our scroll trigger for timeline scrolly
+const ST = ScrollTrigger.create({
+  trigger: ".content-container",
+  start: "top top",
+  end: "bottom bottom",
+  onUpdate: getCurrentSection,
+  pin: ".left-content"
+});
+
+const contentMarkers = gsap.utils.toArray(".contentMarker");
+
+// Set up our content behaviors
+contentMarkers.forEach(marker => {
+  marker.content = document.querySelector(`#${marker.dataset.markerContent}`);
+
+  if(marker.content.tagName === "DIV") {
+    gsap.set(marker.content, {transformOrigin: "center"});
+
+    marker.content.enter = function() {
+      gsap.fromTo(marker.content, {autoAlpha: 0, rotateY: -30}, {duration: 0.3, autoAlpha: 1, rotateY: 0});
+    }
+  } else if(marker.content.tagName === "BLOCKQUOTE") {
+    gsap.set(marker.content, {transformOrigin: "left center"});
+
+    marker.content.enter = function() {
+      gsap.fromTo(marker.content, {autoAlpha: 0, rotateY: 50}, {duration: 0.3, autoAlpha: 1, rotateY: 0});
+    }
+  }
+
+  marker.content.leave = function() {
+    gsap.to(marker.content, {duration: 0.1, autoAlpha: 0});
+  }
+
+});
+
+// Handle the updated position
+let lastContent;
+function getCurrentSection() {
+  let newContent;
+  const currScroll = scrollY;
+
+  // Find the current section
+  contentMarkers.forEach(marker => {
+    if(currScroll > marker.offsetTop) {
+      newContent = marker.content;
+    }
+  });
+
+  // If the current section is different than that last, animate in
+  if(newContent
+  && (lastContent == null
+     || !newContent.isSameNode(lastContent))) {
+    // Fade out last section
+    if(lastContent) {
+      lastContent.leave();
+    }
+
+    // Animate in new section
+    newContent.enter();
+
+    lastContent = newContent;
+  }
+
+}
+
+const media = window.matchMedia("screen and (max-width: 600px)");
+ScrollTrigger.addEventListener("refreshInit", checkSTState);
+checkSTState();
+
+function checkSTState() {
+  if(media.matches) {
+    ST.disable();
+  } else {
+    ST.enable();
+  }
+}
